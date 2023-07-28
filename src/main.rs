7f884ea8a343557452fe074f86155d2091c5399e
Teddy <leungteddy12@gmail.com>
2023-07-29 02:24:12 +0800

    the scanner class 4.4

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,94 @@
 use std::env;
+use std::fmt;
 use std::fs;
 use std::io;
+use std::usize;
+
+pub enum TokenType {
+    // Single character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals
+    Identifier,
+    String,
+    Number,
+
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    EOF,
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+struct Literal {}
+
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Literal,
+    line: i32,
+}
 
 fn main() {
+    let mut had_error = false;
     let args: Vec<String> = env::args().collect();
     if args.len() > 2 {
         println!("Usage: rlox [script]");
     } else if args.len() == 2 {
-        run_file(&args[1]);
+        run_file(&args[1], had_error);
     } else {
-        run_prompt();
+        run_prompt(&mut had_error);
     }
 }
 
-fn run_file(path: &str) {
+fn run_file(path: &str, had_error: bool) {
+    if had_error {
+        panic!();
+    }
     let contents = fs::read_to_string(path).expect("Should have been able to read the file");
     run(contents.as_str());
 }
 
-fn run_prompt() {
+fn run_prompt(had_error: &mut bool) {
     loop {
         let mut prompt = String::new();
         println!("> ");
@@ -30,11 +100,12 @@ fn run_prompt() {
             break;
         }
         run(prompt.as_str());
+        *had_error = false;
     }
 }
 
 fn run(source: &str) {
-    let scanner = Scanner { source };
+    let mut scanner = Scanner::new(source);
     let tokens = scanner.scan_tokens();
 
     for token in tokens {
@@ -44,10 +115,39 @@ fn run(source: &str) {
 
 struct Scanner<'a> {
     source: &'a str,
+    tokens: Vec<TokenType>,
+    start: i32,
+    current: i32,
+    line: i32,
 }
 
 impl<'a> Scanner<'a> {
-    fn scan_tokens(&'a self) -> Vec<&'a str> {
-        vec![self.source]
+    fn new(source: &'a str) -> Self {
+        Self {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
     }
+    fn scan_tokens(&'a mut self) -> &Vec<TokenType> {
+        while !self.is_at_end() {
+            self.start = self.current;
+        }
+        &self.tokens
+    }
+    fn is_at_end(&'a self) -> bool {
+        usize::try_from(self.current)
+            .map(|current| current > self.source.len())
+            .unwrap_or(false)
+    }
+}
+
+fn error(line: i32, message: String) {
+    report(line, String::from(""), message);
+}
+
+fn report(line: i32, at: String, message: String) {
+    println!("[line {line}] Error {at}: {message}");
 }
