83e75ff320656a292ce582145b68743bf09df65c
Theo <theodore.leung@fano.ai>
2023-11-25 18:21:10 +0800

    wip 8.1.3

--- src/stmt.rs
+++ src/stmt.rs
@@ -1,6 +1,6 @@
 use crate::{
     ast::{Expr, Visitor},
-    token::Token,
+    token::{Token, TokenType},
 };
 
 pub enum Stmt {
@@ -40,8 +40,48 @@ pub enum Stmt {
 
 pub struct Statement;
 
-impl Visitor<Stmt, Stmt> for Statement {
-    fn visit_expr(&mut self, stmt: &Stmt) -> Stmt {
+impl Visitor<Stmt, ()> for Statement {
+    fn visit_expr(&mut self, stmt: &Stmt) {
+        match stmt {
+            (_) => {}
+        }
+    }
+}
+
+impl Statement {
+    fn parse(&self) -> Vec<Stmt> {
+        let mut statements: Vec<Stmt> = Vec::new();
+        while (!self.is_at_end()) {
+            statements.push(self.statement());
+        }
+
+        return statements;
+    }
+
+    fn statement(&self) -> Stmt {
+        if (self.matches(TokenType::Print)) {
+            return self.print_statement();
+        }
+        return self.expression_statement();
+    }
+
+    fn print_statement(&self) -> Stmt {
+        let value = self.expression();
+        self.consume(TokenType::Semicolon, "Expect ';' after value.");
+        return Stmt::Print(value);
+    }
+
+    fn expression_statement(&self) -> Stmt {
+        let expr = self.expression();
+        self.consume(TokenType::Semicolon, "Expect ';' after expression.");
+        return Stmt::Expression(expr);
+    }
+
+    fn is_at_end(&self) -> bool {
+        todo!();
+    }
+
+    fn expression(&self) -> Expr {
         todo!();
     }
 }
