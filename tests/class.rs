b08d008f88d7efd5ea5baea03449431986875dca
Teddy <leungteddy12@gmail.com>
2024-08-02 12:21:48 +0700

    add class_1 test, add failing class_2 test

---
+++ tests/class.rs
@@ -0,0 +1,39 @@
+use std::io::Cursor;
+
+use rlox::run_file;
+
+#[test]
+fn class_1() {
+    let mut had_error = false;
+    let mut had_runtime_error = false;
+    let mut writer = Cursor::new(Vec::<u8>::new());
+    run_file(
+        "tests/samples/class_1.txt",
+        &mut writer,
+        &mut had_error,
+        &mut had_runtime_error,
+        false,
+    );
+    let string = String::from_utf8((&writer.get_ref()).to_vec()).expect("Found invalid UTF-8");
+    assert_eq!(string, "Crunch crunch crunch!\n");
+    assert!(!had_error);
+    assert!(!had_runtime_error);
+}
+
+#[test]
+fn class_2() {
+    let mut had_error = false;
+    let mut had_runtime_error = false;
+    let mut writer = Cursor::new(Vec::<u8>::new());
+    run_file(
+        "tests/samples/class_2.txt",
+        &mut writer,
+        &mut had_error,
+        &mut had_runtime_error,
+        false,
+    );
+    let string = String::from_utf8((&writer.get_ref()).to_vec()).expect("Found invalid UTF-8");
+    assert_eq!(string, "The German chocolate cake is delicious!\n");
+    assert!(!had_error);
+    assert!(!had_runtime_error);
+}
