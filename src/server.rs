2ed1693daf0948355c5a47e22280766ac57360a2
theojl6 <leungteddy12@gmail.com>
2024-08-05 23:35:45 +0800

    wip server mode

--- src/server.rs
+++ src/server.rs
@@ -4,12 +4,25 @@ use std::{
 };
 
 pub fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+    let buf_reader = Rc::new(BufReader::new(&mut stream));
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
+    println!("{:?}", http_request);
+    let request_body: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take(2)
+        .collect();
+    println!("{:?}", request_body);
 
-    println!("Request: {http_request:#?}")
+    let status_line = "HTTP/1.1 200 OK";
+
+    let content = "Hello".to_string();
+    let length = content.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
